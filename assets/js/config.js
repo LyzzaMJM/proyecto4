02af522a94af7af5350a66d49dd6ef2cda693b00
Valerianavo <lilaj2sy@gmail.com>
2024-10-21 20:29:39 -0500

    nuevo cambio

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 import { getFirestore, collection, addDoc, getDocs, getDoc, onSnapshot, deleteDoc, doc, updateDoc } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
@@ -27,7 +26,7 @@ export function registerUser(email, password) {
     return createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             console.log("Registro exitoso. ¡Bienvenido!");
-            window.location.href = '../assets/html/casas.html';
+            window.location.href = 'assets/html/principal.html';
         })
         .catch((error) => {
             console.error("Error al registrar:", error.code, error.message);
@@ -41,7 +40,7 @@ export function loginUser(email, password) {
         .then((userCredential) => {
             console.log(userCredential);            
             console.log("Inicio de sesión exitoso. ¡Bienvenido!");
-            window.location.href = '../assets/html/casas.html';
+            window.location.href = 'assets/html/principal.html';
         })
         .catch((error) => {
             console.error("Error al iniciar sesión:", error.code, error.message);
@@ -54,54 +53,10 @@ export function loginWithGoogle() {
     return signInWithPopup(auth, provider)
         .then((result) => {
             console.log("Inicio de sesión con Google exitoso. ¡Bienvenido!", result.user);
-            window.location.href = '../assets/html/casas.html';
+            window.location.href = 'assets/html/principal.html';
         })
         .catch((error) => {
             console.error("Error al iniciar sesión con Google:", error.code, error.message);
             alert("Error al iniciar sesión con Google: " + error.message);
         });
 }
-
-// // Función para agregar una tarea
-// export function saveTask(title, description) {
-//     console.log("Saving task:", title, description);
-//     return addDoc(collection(db, 'tasks'), {
-//         title: title,
-//         description: description
-//     });
-// }
-
-// // Función carga una única vez todas las tareas desde la colección 'tasks'.
-// export function getTasks() {
-//     console.log("Fetching tasks list");
-//     return getDocs(collection(db, 'tasks'));
-// }
-
-// // Función escucha los cambios en tiempo real en la colección 'tasks'.
-// // Crea una suscripción. Cada vez que se agregue, elimine o actualice un documento, el callback se ejecutará automáticamente.
-// export function onGetTasks(callback) {
-//     return onSnapshot(collection(db, 'tasks'), callback);
-// }
-
-// // Función para obtener una tarea específica
-// export function getTask(id) {
-//     console.log("Fetching task:", id);
-//     return getDoc(doc(db, 'tasks', id));
-// }
-
-// // Función para actualizar una tarea
-// export function updateTask(id, newFields) {
-//     console.log("Updating Task:", id);
-//     return updateDoc(doc(db, 'tasks', id), newFields);
-// }
-
-// // Función para eliminar una tarea
-// export function deleteTask(id) {
-//     console.log("Deleting task:", id);
-//     return deleteDoc(doc(db, "tasks", id));
-// }
-
-// // Exportar autenticación y base de datos
-// export { auth };
-
-// // export { auth, db };
