56b4da49f5731db27e0c89babbc7be52fe6a7f11
Valerianavo <lilaj2sy@gmail.com>
2024-10-27 08:44:22 -0500

    sancando trivia del login

--- assets/js/feed.js
+++
@@ -1,6 +0,0 @@
-const fullName = localStorage.getItem('fullName');
-        
-// Muestra el mensaje de bienvenida
-if (fullName) {
-    document.getElementById('welcomeMessage').innerText = ` ${fullName}`;
-}
